5c75ca7b85d1ca87fdbb0b1ccadee53ae174cdd7
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 23:24:28 +0300

    Initial commit

---
+++ DOKUNMA OYUNU/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace DOKUNMA_OYUNU
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyeşiluzun = new System.Windows.Forms.Button();
+            this.btnyeşilkısa = new System.Windows.Forms.Button();
+            this.btnkırmızıuzun = new System.Windows.Forms.Button();
+            this.btnkırmızıkısa = new System.Windows.Forms.Button();
+            this.btnbitiş = new System.Windows.Forms.Button();
+            this.btnengel = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnyeşiluzun
+            // 
+            this.btnyeşiluzun.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnyeşiluzun.Location = new System.Drawing.Point(95, 64);
+            this.btnyeşiluzun.Name = "btnyeşiluzun";
+            this.btnyeşiluzun.Size = new System.Drawing.Size(264, 27);
+            this.btnyeşiluzun.TabIndex = 0;
+            this.btnyeşiluzun.UseVisualStyleBackColor = false;
+            
+            this.btnyeşiluzun.MouseHover += new System.EventHandler(this.btnyeşiluzun_MouseHover);
+            // 
+            // btnyeşilkısa
+            // 
+            this.btnyeşilkısa.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnyeşilkısa.Location = new System.Drawing.Point(95, 31);
+            this.btnyeşilkısa.Name = "btnyeşilkısa";
+            this.btnyeşilkısa.Size = new System.Drawing.Size(297, 27);
+            this.btnyeşilkısa.TabIndex = 1;
+            this.btnyeşilkısa.UseVisualStyleBackColor = false;
+            this.btnyeşilkısa.MouseHover += new System.EventHandler(this.btnyeşilkısa_MouseHover);
+            // 
+            // btnkırmızıuzun
+            // 
+            this.btnkırmızıuzun.BackColor = System.Drawing.Color.Red;
+            this.btnkırmızıuzun.Location = new System.Drawing.Point(356, 64);
+            this.btnkırmızıuzun.Name = "btnkırmızıuzun";
+            this.btnkırmızıuzun.Size = new System.Drawing.Size(27, 228);
+            this.btnkırmızıuzun.TabIndex = 2;
+            this.btnkırmızıuzun.UseVisualStyleBackColor = false;
+            this.btnkırmızıuzun.MouseHover += new System.EventHandler(this.btnkırmızıuzun_MouseHover);
+            // 
+            // btnkırmızıkısa
+            // 
+            this.btnkırmızıkısa.BackColor = System.Drawing.Color.Red;
+            this.btnkırmızıkısa.Location = new System.Drawing.Point(389, 31);
+            this.btnkırmızıkısa.Name = "btnkırmızıkısa";
+            this.btnkırmızıkısa.Size = new System.Drawing.Size(26, 318);
+            this.btnkırmızıkısa.TabIndex = 3;
+            this.btnkırmızıkısa.UseVisualStyleBackColor = false;
+            this.btnkırmızıkısa.MouseHover += new System.EventHandler(this.btnkırmızıkısa_MouseHover);
+            // 
+            // btnbitiş
+            // 
+            this.btnbitiş.BackColor = System.Drawing.Color.Blue;
+            this.btnbitiş.Location = new System.Drawing.Point(342, 400);
+            this.btnbitiş.Name = "btnbitiş";
+            this.btnbitiş.Size = new System.Drawing.Size(50, 15);
+            this.btnbitiş.TabIndex = 4;
+            this.btnbitiş.UseVisualStyleBackColor = false;
+            this.btnbitiş.Click += new System.EventHandler(this.btnbitiş_Click);
+            this.btnbitiş.MouseHover += new System.EventHandler(this.btnbitiş_MouseHover);
+            // 
+            // btnengel
+            // 
+            this.btnengel.BackColor = System.Drawing.Color.Red;
+            this.btnengel.Location = new System.Drawing.Point(389, 377);
+            this.btnengel.Name = "btnengel";
+            this.btnengel.Size = new System.Drawing.Size(26, 60);
+            this.btnengel.TabIndex = 5;
+            this.btnengel.UseVisualStyleBackColor = false;
+            this.btnengel.Click += new System.EventHandler(this.btnengel_Click);
+            this.btnengel.MouseHover += new System.EventHandler(this.btnengel_MouseHover);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.GreenYellow;
+            this.button1.Location = new System.Drawing.Point(253, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 27);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.GreenYellow;
+            this.button2.Location = new System.Drawing.Point(220, 322);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(172, 27);
+            this.button2.TabIndex = 8;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Fuchsia;
+            this.button3.Location = new System.Drawing.Point(231, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 156);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Fuchsia;
+            this.button4.Location = new System.Drawing.Point(198, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(27, 156);
+            this.button4.TabIndex = 10;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.SpringGreen;
+            this.button5.Location = new System.Drawing.Point(95, 160);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(142, 27);
+            this.button5.TabIndex = 11;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.SpringGreen;
+            this.button6.Location = new System.Drawing.Point(126, 193);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(76, 22);
+            this.button6.TabIndex = 12;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightCoral;
+            this.button7.Location = new System.Drawing.Point(95, 183);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(27, 254);
+            this.button7.TabIndex = 13;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.LightCoral;
+            this.button8.Location = new System.Drawing.Point(128, 209);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(27, 195);
+            this.button8.TabIndex = 14;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.LimeGreen;
+            this.button9.Location = new System.Drawing.Point(116, 410);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(276, 27);
+            this.button9.TabIndex = 15;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.LimeGreen;
+            this.button10.Location = new System.Drawing.Point(151, 377);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(241, 27);
+            this.button10.TabIndex = 16;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.MouseHover += new System.EventHandler(this.button3_MouseHover);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Başlangıç -->";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(479, 451);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnengel);
+            this.Controls.Add(this.btnbitiş);
+            this.Controls.Add(this.btnkırmızıkısa);
+            this.Controls.Add(this.btnkırmızıuzun);
+            this.Controls.Add(this.btnyeşilkısa);
+            this.Controls.Add(this.btnyeşiluzun);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dokunma V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnyeşiluzun;
+        private System.Windows.Forms.Button btnyeşilkısa;
+        private System.Windows.Forms.Button btnkırmızıuzun;
+        private System.Windows.Forms.Button btnkırmızıkısa;
+        private System.Windows.Forms.Button btnbitiş;
+        private System.Windows.Forms.Button btnengel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ DOKUNMA OYUNU/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DOKUNMA_OYUNU
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnyeşiluzun_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandın :(");
+        }
+
+        private void btnyeşilkısa_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandın :(");
+        }
+
+        private void btnkırmızıuzun_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandın :(");
+        }
+
+        private void btnkırmızıkısa_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandın :(");
+        }
+
+        private void btnbitiş_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bravo Kazandın :D");
+        }
+
+        private void btnengel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnengel_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hile Yapmayınız!!!");
+        }
+
+        private void btnbitiş_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandın :(");
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandın :(");
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandın :(");
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandın :(");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
